d6f7db86eebed29245725e66d6e594a76883b301
BrieG94 <bagalindo@zonemail.clpccd.edu>
2022-02-10 11:05:15 -0800

    Changed information in code

--- assignment 3-1.cpp
+++ assignment 3-1.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 int main()
 {
+int classA;
+int classB;
+int ClassC;
 
+int priceA = 15;
+int priceB = 12;
+int priceC = 9;
+
+cout <<"Enter the number of tickets sold for class A: ";
+cin >> classA;
+cout << "Enter the number of tickets sold for class B: ";
+cin >> classB;
+cout << "Enter the number of tickets sold for class C: ";
+cin >> classC;
+
+total = (classA * priceA ) + (classB + priceB ) + (classC + priceC );
+
+cout << "Total price for all tickets sold" << total << endl;
 
 }
